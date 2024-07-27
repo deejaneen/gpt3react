07218d9cc323b74934916bf3f71fe49435096fcc
deeja-ub <tomeldendjaninetara@gmail.com>
2024-07-27 11:12:41 +0800

    added id

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -5,7 +5,7 @@ import './blog.css'
 
 const Blog = () => {
   return (
-    <div className='gpt3__blog section__padding'>
+    <div className='gpt3__blog section__padding' id='blog'>
       <div className='gpt3__blog-heading'>
         <h1 className='gradient__text'>A lot is happening, 
         We are blogging about it.</h1>
